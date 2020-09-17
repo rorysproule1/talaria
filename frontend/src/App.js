ed8c30d8882564a8be27fef6aad45039f0abcb54
Rory Sproule <rory.sproule@outlook.com>
2020-09-17 23:52:56 +0100

    Adding MyProfile button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios";
 import SignInSide from "./SignIn/SignIn";
 import Dashboard from "./Dashboard/Dashboard";
