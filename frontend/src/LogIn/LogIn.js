ac070ded3d87617393a33f0ebee4e56e9e804b55
Rory Sproule <rory.sproule@outlook.com>
2020-09-20 02:45:36 +0100

    POST for access token

--- frontend/src/LogIn/LogIn.js
+++ frontend/src/LogIn/LogIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
@@ -10,16 +10,15 @@ import Grid from "@material-ui/core/Grid";
 import StarIcon from "@material-ui/icons/StarBorder";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import AssignmentIcon from "@material-ui/icons/Assignment";
 import TimelineIcon from "@material-ui/icons/Timeline";
 import CreateIcon from "@material-ui/icons/Create";
-import powered_by_strava from "./powered_by_strava.png";
-import strava_connect from "./btn_strava_connect.png";
-import talaria_logo from "./logo-talaria.png";
-import talaria_logo_circle from "./logo-talaria-circle.png";
+import powered_by_strava from "../assets/images/powered_by_strava.png";
+import strava_connect from "../assets/images/btn_strava_connect.png";
+import talaria_logo_circle from "../assets/images/logo-talaria-circle.png";
+import axios from "axios";
 
 function Copyright() {
   return (
@@ -120,6 +119,47 @@ export default function Login() {
     "https://www.strava.com/oauth/authorize?client_id=52053&redirect_uri=http://localhost:3000/login&response_type=code&scope=activity:read_all";
   const [connectToStrava, setConnectToStrava] = useState(false);
 
+  useEffect(() => {  
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    var code = url.searchParams.get("code");
+    var scope = url.searchParams.get("scope");
+
+    if (code && scope.includes("activity:read_all")) {
+      const token_url = "https://www.strava.com/oauth/token"
+      const post_data = {
+        client_id: 52053,
+        client_secret: "652aa8ebedc48c9fcf061fb28f663b6eca0669a6",
+        code: code,
+        grant_type: "authorization_code",
+      }
+
+      axios.post(token_url, post_data, {})
+      .then((response) => {
+        console.log(response.data)
+        var access_token = response.data["access_token"]
+        var refresh_token = response.data["refresh_token"]
+        var currentDate = new Date()
+        currentDate.setSeconds(currentDate.getSeconds() + response.data["expires_in"])
+        var expires_at = currentDate
+
+        var athlete_id = response.data["athlete"]["id"]
+        var first_name = response.data["athlete"]["firstname"]
+        var last_name = response.data["athlete"]["lastname"]
+        var sex = response.data["athlete"]["sex"]
+
+        console.log(access_token + "\n" + refresh_token + "\n" + expires_at)
+        console.log(athlete_id + "\n" + first_name + "\n" + last_name)
+      })
+      .catch((error) => {
+        console.error('Error while posting for token')
+        console.log(error)
+      })
+    }
+    
+
+  }, []); // empty list to ensure code is only executed on initial loading of the page
+
   function onClickHandler(e) {
     setConnectToStrava(true);
 
@@ -203,13 +243,13 @@ export default function Login() {
                 <CardContent>
                   <div className={classes.cardPricing}>
                     {tier.title === "Plan" && (
-                      <CreateIcon fontSize="large" color="secondary" />
+                      <CreateIcon fontSize="large" color="primary" />
                     )}
                     {tier.title === "Track" && (
-                      <AssignmentIcon fontSize="large" color="secondary" />
+                      <AssignmentIcon fontSize="large" color="primary" />
                     )}
                     {tier.title === "Analyse" && (
-                      <TimelineIcon fontSize="large" color="secondary" />
+                      <TimelineIcon fontSize="large" color="primary" />
                     )}
                   </div>
                   <ul>
